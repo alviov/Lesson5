a6bb46c76e346b68b7f70c58fedfa8dc6e5dc250
Саша Овчинников <alviov@ro.ru>
2022-05-08 22:27:16 +0700

    Vektor handling

---
+++ Lesson5/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задаём два вектора с рандомными координатами
+            Random rnd = new Random();
+            Vector vector1 = new Vector(rnd.NextDouble() * 10, rnd.NextDouble() * 10, rnd.NextDouble() * 10);
+            Vector vector2 = new Vector(rnd.NextDouble() * 10, rnd.NextDouble() * 10, rnd.NextDouble() * 10);
+
+            //выводим данные первого вектора (координаты и длина)
+            Console.WriteLine("randomly generated vector1:");
+            vector1.DisplayVectorInfo();
+            Console.WriteLine("vector1 length is:");
+            vector1.DisplayVectorLength();
+
+            //выводим данные второго вектора (координаты и длина)
+            Console.WriteLine();
+            Console.WriteLine("randomly generated vector2:");
+            vector2.DisplayVectorInfo();
+            Console.WriteLine("vector2 length is:");
+            vector2.DisplayVectorLength();
+
+            //выводим скалярное произведение
+            Console.WriteLine();
+            vector1.DisplayScalarMultiplication(vector2);
+
+            //рассчитываем и выводим векторное произведение
+            Console.WriteLine();
+            Vector vector3 = VectorMultiplication(vector1, vector2);
+            Console.WriteLine("vector1 and vector2 vector multiplication is vector3:");
+            vector3.DisplayVectorInfo();
+
+            //рассчитываем и выводим угол между векторами
+            Console.WriteLine();
+            vector1.DisplayVectorAngle(vector2);
+
+            //рассчитываем и выводим сумму векторов
+            Console.WriteLine();
+            Vector vector4 = VectorMultiplication(vector1, vector2);
+            Console.WriteLine("vector1 and vector2 sum is vector4:");
+            vector4.DisplayVectorInfo();
+
+            //рассчитываем и выводим разность векторов
+            Console.WriteLine();
+            Vector vector5 = VectorMultiplication(vector1, vector2);
+            Console.WriteLine("vector1 and vector2 subtraction is vector5:");
+            vector5.DisplayVectorInfo();
+
+            Console.ReadKey();
+        }
+
+        //метод для расчёта вектора - векторного произведения
+        public static Vector VectorMultiplication(Vector vector1, Vector vector2)
+        {
+            double xm = vector1.y * vector2.z - vector1.z * vector2.y;
+            double ym = vector1.z * vector2.x - vector1.x * vector2.z;
+            double zm = vector1.x * vector2.y - vector1.y * vector2.x;
+            return new Vector(xm, ym, zm);
+        }
+
+        //метод для расчёта вектора - векторного сложения
+        public static Vector VectorSum(Vector vector1, Vector vector2)
+        {
+            double xs = vector1.x + vector2.x;
+            double ys = vector1.y + vector2.y;
+            double zs = vector1.z + vector2.z;
+            return new Vector(xs, ys, zs);
+        }
+        //метод для расчёта вектора - векторной разности
+        public static Vector VectorSubtraction(Vector vector1, Vector vector2)
+        {
+            double xsub = vector1.x - vector2.x;
+            double ysub = vector1.y - vector2.y;
+            double zsub = vector1.z - vector2.z;
+            return new Vector(xsub, ysub, zsub);
+        }
+
+    }
+}
---
+++ Lesson5/Vector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson5
+{
+    public class Vector
+    {
+        public double x;
+        public double y;
+        public double z;
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public void DisplayVectorInfo()
+        {
+            Console.WriteLine($"x: {x.ToString("F")} y: {y.ToString("F")} z: {z.ToString("F")}");
+        }
+        public void DisplayVectorLength()
+        {
+            Console.WriteLine(Math.Sqrt(x * x + y * y + z * z).ToString("F"));
+        }
+        public void DisplayScalarMultiplication(Vector b)
+        {
+            Console.WriteLine("Scalar multiplication of vector1 and vector2 is:");
+            Console.WriteLine(Math.Sqrt(x * b.x + y * b.y + z * b.z).ToString("F"));
+        }
+        public void DisplayVectorAngle(Vector b)
+        {
+            Console.WriteLine("Vector1 and vector2 form angle:");
+            Console.WriteLine((Math.Sqrt(x * b.x + y * b.y + z * b.z) / (Math.Sqrt(x * x + y * y + z * z) * Math.Sqrt(b.x * b.x + b.y * b.y + b.z * b.z))).ToString("F"));
+        }
+    }
+}
